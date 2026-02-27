20d8acd59b65866a3abbf964630a364432f288a4
Mariam alaa <mariamalaa203030@gmail.com>
2026-02-27 14:29:55 +0200

    Add admin and transaction routes to App.js

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const contractRoutes = require('./routes/contractRoutes');
 const searchRoutes = require('./routes/searchRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const transactionRoutes = require('./routes/transactionRoutes');
 
 const path = require('path');
 
@@ -26,8 +28,8 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/users', userRoutes);
 app.use('/api/contracts', contractRoutes);
 app.use('/api/search', searchRoutes);
-
+app.use('/api/admin', adminRoutes);
+app.use('/api/transactions', transactionRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
